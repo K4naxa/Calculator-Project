66ebda94d0cdd470f9583f2536fde32c107cd3e8
K4naxa <K4na@iki.fi>
2022-12-16 09:51:00 +0200

    made basic calculator layout

---
+++ script.js
@@ -0,0 +1,20 @@
+const window = document.querySelector(".window");
+
+const resetBtn = document.getElementById("resetBtn");
+
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num2");
+const num3 = document.getElementById("num3");
+const num4 = document.getElementById("num4");
+const num5 = document.getElementById("num5");
+const num6 = document.getElementById("num6");
+const num7 = document.getElementById("num7");
+const num8 = document.getElementById("num8");
+const num9 = document.getElementById("num9");
+const num0 = document.getElementById("num0");
+
+const add = document.getElementById("add");
+const subtract = document.getElementById("subtract");
+const multiply = document.getElementById("multiply");
+const divide = document.getElementById("divide");
+const sum = document.getElementById("sum");
